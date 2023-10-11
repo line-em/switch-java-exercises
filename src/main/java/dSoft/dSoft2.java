c3ab57319a405eccfe85468f241cf21a4f457cf2
Line-em <alineemily@hotmail.com>
2023-10-12 00:19:22 +0100

    fix exercise 01 dSoft02, removing uneeded check, and cleaning up the returns

--- src/main/java/dSoft/dSoft2.java
+++ src/main/java/dSoft/dSoft2.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 public class dSoft2 {
 	/////////////// QUESTION 01
-	// a. Esse método e utilizado para calcular a média de notas de um aluno,
-	// supondo tres provas diferentes, com diferentes pesos na média final.
+	// a. Esse método é utilizado para calcular a média de notas de um aluno,
+	// supondo provas diferentes, com diferentes pesos na média final.
 	public static double getAverage(int[] grades, int[] weights) {
 		if (grades.length != weights.length) return -1; //Should have same length
 
@@ -13,13 +13,12 @@ public class dSoft2 {
 		double totalGrades = 0;
 
 		for (int i = 0; i < grades.length; i++) {
-			if (weights[i] < 0 || grades[i] < 0) return -1; //Should not be negative
+			if (weights[i] <= 0 || grades[i] < 0) return -1; //Should not be negative
 			totalWeight += weights[i];
 			totalGrades += grades[i] * weights[i];
 		}
 
-		if (totalWeight > 0) return (totalGrades / totalWeight);
-		else return 0;
+		return totalGrades / totalWeight;
 	}
 
 	public static boolean hasPassed(double studentAverage) {
@@ -43,12 +42,13 @@ public class dSoft2 {
 		} else {
 			return null;
 		}
-
 	}
 
 	public static boolean isEven(int number) {
 		return (number % 2 == 0);
 	}
+
+	//	TODO: Criar uma função para juntar os dois.
 
 	/////////////// QUESTION 03
 	public static double getDistanceBetweenPoints(int x1, int x2, int y1, int y2) {
